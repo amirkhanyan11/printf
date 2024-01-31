16861b15b06079450bdb804a8070f5627052b90e
Artyom Amirkhanyan <aamirkha@c2r7s4.42yerevan.am>
2024-01-31 21:58:33 +0400

    asdasdasdas

--- includes/libftprintf.h
+++ includes/libftprintf.h
@@ -6,13 +6,14 @@
 /*   By: aamirkha <aamirkha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 20:01:50 by aamirkha          #+#    #+#             */
-/*   Updated: 2024/01/31 20:02:51 by aamirkha         ###   ########.fr       */
+/*   Updated: 2024/01/31 21:55:19 by aamirkha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFTPRINTF_H
 # define LIBFTPRINTF_H
 
-# include "../libft/libft.h"
+# include "libft.h"
+# include <stdarg.h>
 
 #endif // LIBFTPRINTF_H
