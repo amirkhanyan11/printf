ef280b4c9d702b0a2147b53bb77bc3b1b9fc3cb1
Artyom Amirkhanyan <aamirkha@c2r5s6.42yerevan.am>
2024-02-01 20:36:55 +0400

    ++norm

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: aamirkha <aamirkha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 20:01:50 by aamirkha          #+#    #+#             */
-/*   Updated: 2024/02/01 20:31:08 by aamirkha         ###   ########.fr       */
+/*   Updated: 2024/02/01 20:36:41 by aamirkha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,15 +22,15 @@ typedef enum e_InputType
 	ValidInput = 0
 }	t_InputType;
 
-int ft_printf(char const *str, ...);
-int ft_parcer(char c, va_list arglist);
+int	ft_printf(char const *str, ...);
+int	ft_parcer(char c, va_list arglist);
 int	ft_putchar(char c);
 int	ft_putstr(char *str);
-int	ft_putnbr(unsigned int const nbr, char const * const base);
+int	ft_putnbr(unsigned int const nbr, char const *const base);
 int	ft_putnbr_signed(long long n);
-int	printnb(int nb, char const * const base);
-int	ft_putptr(unsigned long long const nbr, char const * const base);
-int	ft_input_handler(int const tmp, const char * const str);
+int	printnb(int nb, char const *const base);
+int	ft_putptr(unsigned long long const nbr, char const *const base);
+int	ft_input_handler(int const tmp, const char *const str);
 
 //int	ft_putptr(unsigned long long const nbr, char const * const base);
 
